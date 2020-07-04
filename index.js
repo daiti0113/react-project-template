444388c69337acd80112eaedff0eb25f9ad426ba
daiti0113 <stennis.ds.0113@gmail.com>
2020-07-04 21:50:21 +0900

    fix:Deleted Input component to prepare a template

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {Header} from "./src/components/Header"
-import {Input} from "./src/components/Input"
 
 const App = () => {
     return (
         <>
             <Header />
-            <Input />
         </>
     )
 }
